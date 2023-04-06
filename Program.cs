5f36340013851a78032f231d96cd35c8deda11c9
Mert Ernaz <rnzappdroid@gmail.com>
2023-04-06 05:45:10 +0300

    arraylist new

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            ArrayList arList=new ArrayList();
+            arList.Add("a");
+            arList.Add("b");
+            arList.Add(1);
+            arList.Add(true);
+            arList.Add(-1);
+            arList.Add(3.5f);
+
+            foreach(var x in arList){
+                Console.WriteLine(x);
+            }
+
+        /*    arList.Sort(); // aynı tip element var ise çalışır (int)
+            foreach(var x in arList){
+                Console.WriteLine(x);
+            }*/
+
+            Console.WriteLine(arList.BinarySearch(1));
+             Console.WriteLine("***********************************");
+
+            string[] isimler={"Ali","Ahmet","Ayşe"};
+            arList.AddRange(isimler);
+
+            foreach(var x in arList){
+                Console.WriteLine(x);
+            }
+
+            arList.Clear();
+            Console.WriteLine("cap="+arList.Count);
+        }
+
+    }
+
+}
